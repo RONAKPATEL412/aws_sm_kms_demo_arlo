8b03b8e9be32f6bf3f588e1db91be170ba125b62
unknown <Administrator@volansys.com>
2022-07-05 18:36:47 +0530

    change key id and key name

--- examples/dummy-secret/main.tf
+++ examples/dummy-secret/main.tf
@@ -8,7 +8,7 @@ module "secret" {
   name  = "test-aws-sm"
   value = "rokkkk3343"
   // policy     = data.template_file.example.rendered            # Optional
-  kms_key_id = "arn:aws:kms:aws-region:account-id:key/key-id" # Optional
+  // kms_key_id = "arn:aws:kms:aws-region:account-id:key/key-id" # Optional
   tags = {
     whodunnit = "dhruv"
     why       = "testing"
